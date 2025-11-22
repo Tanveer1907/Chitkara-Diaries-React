5187e801fbca75ceef71dda44471132781efcd34
Ruhani Bajaj <bajaj.ruhani1404@gmail.com>
2025-11-22 21:54:44 +0530

    app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
+// src/App.jsx
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home/homepage";
 import Auth from "./pages/Auth/Auth";
+import VotingPg from "./pages/Voting/Voting_pg"; // <-- add this import
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/auth/*" element={<Auth />} />
+        <Route path="/voting" element={<VotingPg />} />   {/* <-- add this route */}
         <Route path="/*" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
